15d6292fa59ef373fab16c4d34ab7f255dffda41
kaihitwari1996-ui <kaihitwari1996@gmail.com>
2026-08-06 05:15:17 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,10 +33,13 @@ android {
         jvmTarget = "1.8"
     }
 
-        buildTypes {
+            buildTypes {
         release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 }
